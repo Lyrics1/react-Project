e89ca4c2d56d12b3939a55619e468eae5321efe6
Lyrics1 <1391302249@qq.com>
2018-02-01 19:44:13 +0800

    完成TabBar切换

--- reactApp/src/component/dashboard/dashboard.js
+++ reactApp/src/component/dashboard/dashboard.js
@@ -1,17 +1,77 @@
 import React from 'react'
-
+import { connect} from 'react-redux'
+import { NavBar} from 'antd-mobile';
+import  NavLinkBar  from '../navlink/navlink'
+function Boss(){
+	return <h1>Boss</h1>
+}
+function Genius(){
+	return <h1>Genius</h1>
+}
+function Msg(){
+	return <h1>Msg</h1>
+}
+function User(){
+	return <h1>User</h1>
+}
+@connect(
+	state=>state
+	)
 class Dashboard extends React.Component{
 	constructor(props) {
 	  super(props);
 	
 	  this.state = {};
 	}
+	
+	
 
 	render(){
+		const { pathname } = this.props.location
+		
+		const user = this.props.user
+		console.log( this.props)
+		const navList =[
+		{
+			path:'/boss',
+			text:'牛人',
+			icon:'boss',
+			title:'牛人列表',
+			component:Boss,
+			hide:user.type == 'genuis'
+			
+		},
+		{
+			path:'/genuis',
+			text:'Boss',
+			icon:'job',
+			title:'Boss列表',
+			component:Genius,
+			hide:user.type == 'boss'
+			
+		},
+		{
+			path:'/msg',
+			text:'消息',
+			icon:'msg',
+			title:'消息列表',
+			component:Msg
+			
+		},
+		{
+			path:'/me',
+			text:'我',
+			icon:'user',
+			title:'个人中心',
+			component:User
+			
+		}
+	]
 		return (
 			<div>
-				Dashboard
-
+				<NavBar mode='dard'>{navList.find(v=>v.path== pathname).title}</NavBar>
+				gggg
+				<NavLinkBar data={navList}></NavLinkBar>
 			</div>
 			)
 	}
---
+++ reactApp/src/component/navlink/navlink.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { TabBar } from 'antd-mobile'
+import { withRouter } from  'react-router-dom'
+@withRouter
+ class NavLinkBar extends React.Component{
+
+	 static propTypes = {
+		data:PropTypes.array.isRequired
+	 }
+ 	constructor(props) {
+ 	  super(props);
+ 	
+ 	  this.state = {};
+ 	}
+
+ 	render(){
+ 		const navList = this.props.data.filter(v=>!v.hide)//过滤掉hide为true的
+ 		const {pathname} =  this.props.location
+ 		return (
+			<TabBar >
+				{navList.map(v=>(
+					<TabBar.Item
+						key={v.path}
+						title={v.text}
+						icon={{uri:require(`./navimg/${v.icon}.png`)}}
+						selectedIcon={{uri:require(`./navimg/${v.icon}-active.png`)}}
+						selected={pathname === v.path}
+						onPress={()=>{
+							this.props.history.push(v.path)
+						}}
+					>	
+				</TabBar.Item>
+				))}
+			</TabBar>
+ 			)
+ 	}
+ }
+
+ export default NavLinkBar
